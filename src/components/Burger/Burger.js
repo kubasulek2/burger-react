adafbb206fbe120acaee778792423f38b64c674e
Kuba Sukowski <kubasulek2@gmail.com>
2019-08-23 20:16:09 +0200

    modified strings in ingredients array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,9 +6,32 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
 
+	/* Create Array from Object Keys */	
+	const ingredients = Object.keys(props.ingredients);
+	
+	/* change each element in array from ie 'burger-top' to 'BurgerTop */
+	
+	ingredients.forEach((str, i, arr) => {
+
+		let result = str.charAt(0).toUpperCase() + str.slice(1);
+		
+		if (str.includes('-')) {
+			result = result.replace(/-/g, '');
+			result = result.split('');
+			result[str.indexOf('-')] = result[str.indexOf('-')].toUpperCase();
+			result = result.join('');
+		}
+		arr[i] = result;
+	});
+
+
 	return (
 		<div className={styles.Burger}>
-			<BurgerIngredient type='BreadTop'/>
+			<BurgerIngredient type='BreadTop' />
+			<BurgerIngredient type='Cheese' />
+			<BurgerIngredient type='Meat' />
+			<BurgerIngredient type='Salad' />
+			<BurgerIngredient type='BreadBottom' />
 		</div>
 	);
 };

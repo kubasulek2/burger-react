228306fe16f92f31d5cfa4c0021a432e52c5f1f9
Kuba Sukowski <kubasulek2@gmail.com>
2019-08-29 12:59:53 +0200

    fetching orders from database

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -11,7 +11,7 @@ export class Orders extends Component {
 	componentDidMount() {
 		axios.get('/orders.json')
 			.then(res => {
-				console.log(res);
+				console.log(res.data);
 				this.setState({loading: false});
 			})
 			.catch(err => {

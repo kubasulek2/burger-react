77fbcf78f8ad6222927f7f33c2c7c8f6b590664b
Kuba Sukowski <kubasulek2@gmail.com>
2019-08-29 13:28:23 +0200

    feefing order with data from database

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,28 +2,54 @@ import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
 import axios from '../../axios-order';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 export class Orders extends Component {
 	state = {
 		orders: [],
-		loading: true
+		loading: true,
+		error: false
 	}
 	componentDidMount() {
 		axios.get('/orders.json')
 			.then(res => {
-				console.log(res.data);
-				this.setState({loading: false});
+				const ordersArr = Object.values(res.data);
+
+			/* 	this.setState({
+					loading: false,
+					orders: ordersArr
+				}); */
 			})
 			.catch(err => {
-				this.setState({loading: false});
+				this.setState({ loading: false });
+				return err;
 			});
 	}
 
 	render() {
+
+		let orders = JSON.parse(JSON.stringify(this.state.orders))
+			.map((order, i) => (
+				<Order key={i} details={order} />
+			));
+
+		if (!orders.length) orders = (
+			<h1
+				style={{
+					textAlign: 'center',
+					fontSize: '2.5rem',
+					margin: '2rem'
+				}}
+			>
+				There are no orders available.
+			</h1>
+		);
+
+		console.log(orders);
 		return (
 			<div>
-				<Order />
-				<Order />
+				{this.state.loading ? <Spinner /> : orders}
+				
 			</div>
 		);
 	}

bd1cb8008a99b666bd9e73b1c1bedb737631ff32
Kuba Sukowski <kubasulek2@gmail.com>
2019-09-02 18:52:54 +0200

    Auth form created

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,124 @@
 import React, { Component } from 'react';
 
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+
+import styles from './Auth.module.css';
+
 export class Auth extends Component {
+	state = {
+		controls: {
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Your Email'
+				},
+				value: '',
+				validation: {
+					required: true,
+					isEmail: true,
+					minLength: 5
+				},
+				valid: false,
+				touched: false
+			},
+			password: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'password',
+					placeholder: 'Password'
+				},
+				value: '',
+				validation: {
+					required: true,
+					minLength: 6
+				},
+				valid: false,
+				touched: false
+			}
+		},
+		isFromValid: false
+	}
+
+	checkValidity = (value, rules) => {
+		let isValid = true;
+		if (rules.required) {
+			isValid = value.trim() !== '' && isValid;
+		}
+
+		if (rules.minLength) {
+			isValid = value.length >= rules.minLength && isValid;
+		}
+
+		if (rules.isEmail) {
+			const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+			isValid = pattern.test(value) && isValid;
+		}
+
+
+		return isValid;
+	}
+
+	inputChangeHandler = (event, inputIdentifier) => {
+
+		const updatedControls = { ...this.state.controls };
+		const updatedControlsElement = { ...updatedControls[inputIdentifier] }; // second level copy
+
+		updatedControlsElement.value = event.target.value;
+		updatedControlsElement.touched = true;
+		updatedControlsElement.valid = this.checkValidity(updatedControlsElement.value, updatedControlsElement.validation);
+		updatedControls[inputIdentifier] = updatedControlsElement;
+
+		let formIsValid = true;
+		
+		/* eslint-disable no-unused-vars */
+
+		for (const input in updatedControls) {
+			formIsValid = updatedControls[input].valid && formIsValid;
+		}
+
+
+		this.setState({
+			controls: updatedControls,
+			isFromValid: formIsValid
+		});
+
+	}
+
+
 	render() {
+
+		const formElementsArr = [];
+
+		for (const key in this.state.controls) {
+			formElementsArr.push({
+				id: key,
+				config: this.state.controls[key]
+			});
+
+		}
+		/* eslint-enable no-unused-vars */
+		const form = formElementsArr.map(input => (
+			<Input
+				key={input.id}
+				elementType={input.config.elementType}
+				elementConfig={input.config.elementConfig}
+				value={input.config.value}
+				invalid={!input.config.valid}
+				touched={input.config.touched}
+				changed={(event) => this.inputChangeHandler(event, input.id)}
+			/>
+		));
+
 		return (
 			<div>
-				
+				<form  className={styles.Auth}>
+					{form}
+					<Button type='Success' disabled={!this.state.isFromValid}>
+						Submit
+					</Button>
+				</form>
 			</div>
 		);
 	}
